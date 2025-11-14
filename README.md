894893c81ccf8bb72a0d72f10b03b05a2124870f
makajda <mcjdaaa@gmail.com>
2025-11-14 19:58:45 +0300

    del blazor

--- README.md
+++
@@ -1,1 +0,0 @@
-https://makajda.github.io/resume/
--- Resume/Common/Settings.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Resume.Common;
-public class Settings {
-    public bool Language { get; set; } = true; // true for Russian, false for English
-
-    public string LnString(string ru, string en) => Language ? ru : en;
-}
